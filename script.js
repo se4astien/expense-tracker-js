fb2be4a16ecdbddcbab06bae3465f7f36935c00b
sebv <sebv@sv.home>
2020-04-02 18:06:39 +0200

    display balance, income and expense

--- script.js
+++ script.js
@@ -13,28 +13,67 @@ const transactions = [
   { id: 4, text: 'Camera', amount: -50 }
 ];
 
-console.log(transactions);
-
 // Add transaction into the DOM
 function addTransactionDOM(transaction) {
-  // Get sign + or -
+  // Add sign - or +
   const sign = transaction.amount < 0 ? '-' : '+';
-  // Add item
+  // 1. Create li element
   const item = document.createElement('li');
-  // Add class based on value
+  // 2. Add class minus or plus
   item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
-  // content on li
+  // 3. Fill content
   item.innerHTML = `${transaction.text} <span>${sign}${Math.abs(
     transaction.amount
   )}</span> <button class="delete-btn">x</button>`;
-  // Append Child to the ul
+  // 4. Append to ul
   list.appendChild(item);
+}
+
+// Update the balance, income and expense
+function updateValues() {
+  //   const amounts = transactions.map(transaction => {
+  //     return transaction.amount;
+  //   });
+  const amounts = transactions.map(transaction => transaction.amount);
+  // console.log(amounts);
+
+  // Total
+  const total = amounts
+    .reduce((accumulator, item) => (accumulator += item), 0)
+    .toFixed(2);
+  // console.log(total);
+  // Add DOM
+  balance.innerHTML = `${total}`;
+
+  // Income
+  const income = amounts
+    .filter(item => item > 0)
+    .reduce((accumulator, item) => (accumulator += item), 0)
+    .toFixed(2);
+  // console.log(income);
+  // Add DOM
+  moneyPlus.innerHTML = `${income}€`;
+
+  // Expense
+  const expense = (
+    amounts
+      .filter(item => item < 0)
+      .reduce((accumulator, item) => (accumulator += item), 0) * -1
+  ).toFixed(2);
+  //   console.log(expense);
+  // Add DOM
+  moneyMinus.innerHTML = `${expense}€`;
 }
 
 // Init App
 function init() {
+  // Clear all items
   list.innerHTML = '';
+  // Loop though the list items
   transactions.forEach(addTransactionDOM);
+  updateValues();
 }
-
 init();
+
+// Add transaction
+form.addEventListener('submit', addTransaction);
